321c35445017dc972b9541b172d805139ad3d071
priyasharma-512 <sbpriya512@github.com>
2022-12-01 23:15:41 +0530

    second commit

--- src/main/java/com/example/RestApiController/UserController.java
+++ src/main/java/com/example/RestApiController/UserController.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class UserController {
@@ -23,37 +20,11 @@ public class UserController {
         return new ResponseEntity(userService.getAllUsers(),HttpStatus.ACCEPTED);
     }
 
-//    @PostMapping("/add_user")
-//    public void addUser(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("age") int age,
-//                        @RequestParam("country") String country){
-//
-//        User user = new User(id,name,country,age);
-//        users.put(id,user);
-//    }
-
     @PostMapping("/add_user_body")
     public ResponseEntity addUserBody(@RequestBody(required = true)User u)
     {
         userService.addUserToDB(u);
         return new ResponseEntity(HttpStatus.CREATED);
     }
-
-//    @GetMapping("/get_user/{id}")
-//    public User getUser(@PathVariable("id") int x)
-//    {
-//        return users.get(x);
-//    }
-
-//    @DeleteMapping("/del_user/{id}")
-//    public void deleteUser(@PathVariable("id") int idx)
-//    {
-//        users.remove(idx);
-//    }
-//
-//    @PutMapping("/update/{id}")
-//    public void updateUser(@PathVariable("id") int id,@RequestBody()User user)
-//    {
-//        users.put(id,user);
-//    }
 
 }
