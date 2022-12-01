b4ca1f2ee12c6439d18d4252309add102dddcbcb
priyasharma-512 <sbpriya512@github.com>
2022-12-01 23:10:56 +0530

    committed

--- src/main/java/com/example/RestApiController/User.java
+++ src/main/java/com/example/RestApiController/User.java
@@ -1,17 +1,25 @@
 package com.example.RestApiController;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
 public class User {
 
     private int id;
     private String name;
-    private int age;
     private String country;
+    private int age;
 
-    public User(int id, String name, int age, String country) {
+    public User(){
+        System.out.println("User object created");
+    }
+
+    public User(int id, String name, String country, int age) {
         this.id = id;
         this.name = name;
-        this.age = age;
         this.country = country;
+        this.age = age;
     }
 
     public int getId() {
@@ -30,19 +38,19 @@ public class User {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getCountry() {
         return country;
     }
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
